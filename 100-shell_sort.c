a4cdf9aaf4e2ffa11801cb755adf4f657116860c
Omondi Were <franklinegift@gmail.com>
2022-07-23 12:10:32 -0700

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,38 @@
+#include "sort.h"
+
+/**
+ * shell_sort - sort using shell sort
+ *
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: Nothing!
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i, j;
+	int tmp;
+
+	if (array == NULL || size < 2)
+		return;
+
+	while (gap < size / 3)
+		gap = gap * 3 + 1;
+
+	while (gap > 0)
+	{
+		for (i = hap; i < size; i++)
+		{
+			tmp = array[i];
+			j = i;
+			while (j >= gap && array[j - gap] > tmp)
+			{
+				array[j] = array[j - gap];
+				j -= gap;
+			}
+			array[j] = tmp;
+		}
+		print_array(array, size);
+		gap /= 3;
+	}
+}
