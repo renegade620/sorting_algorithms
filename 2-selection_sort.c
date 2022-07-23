d2216df799b416b08245744871d98fb7ab55b82d
Omondi Were <franklinegift@gmail.com>
2022-07-23 11:24:09 -0700

    selection

--- 2-selection_sort.c
+++ 2-selection_sort.c
@@ -14,3 +14,34 @@ void swap(int *a, int *b)
 	*a = *b;
 	*b = tmp;
 }
+
+/**
+ * selection_sort - sorts an array of integers in ascending order
+ * using the Selection sort algorithm
+ *
+ * @array: array of integers
+ * @size: size of array
+ *
+ * Return: Nothing!
+ */
+void selection_sort(int *array, size_t size)
+{
+	int *x;
+	size_t i, j;
+
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		x = array + i;
+		for (j = i + 1; j < size; j++)
+			x = (array[j] < *x) ? (array + j) : x;
+
+		if ((array + i) != x)
+		{
+			swap(array + i, x);
+			print_array(array, size);
+		}
+	}
+}
